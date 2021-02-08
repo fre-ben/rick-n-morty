862b80f94d8f65f7a55a65510ed1453c189e6e48
fre-ben <fschocke@gmail.com>
2021-02-08 17:14:09 +0100

    Add cards

---
+++ src/components/cards/card.js
@@ -0,0 +1,34 @@
+import { createElement } from "../../utils/createElement";
+
+export function createCard({ imgSrc, name, status, species, origin }) {
+  return createElement("div", {
+    className: "card",
+    children: [
+      createElement("img", {
+        className: "card__portrait",
+        src: imgSrc,
+      }),
+      createElement("div", {
+        className: "card__info",
+        children: [
+          createElement("h2", {
+            className: "card__name",
+            innerText: name,
+          }),
+          createElement("p", {
+            className: "card__status",
+            innerText: `${status === "Alive" ? "😀" : "💀"} - ${status}`,
+          }),
+          createElement("p", {
+            className: "card__species",
+            innerText: species,
+          }),
+          createElement("p", {
+            className: "card__origin",
+            innerText: origin.name,
+          }),
+        ],
+      }),
+    ],
+  });
+}
---
+++ src/components/cards/cards.stories.js
@@ -0,0 +1,25 @@
+import "./card.css";
+import { createCard } from "./card";
+
+export default {
+  title: "Components/Cards",
+  parameters: { layout: "centered" },
+};
+
+export const Rick = () =>
+  createCard({
+    imgSrc: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    name: "Rick Sanchez",
+    status: "Alive",
+    species: "Human",
+    origin: { name: "Earth (C-137)" },
+  });
+
+export const Morty = () =>
+  createCard({
+    imgSrc: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    name: "Morty Smith",
+    status: "Dead",
+    species: "Human",
+    origin: { name: "Earth (C-137)" },
+  });
