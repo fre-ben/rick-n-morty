b8dc4cd61238a160a70698bb0dba07588b1e6633
fre-ben <fschocke@gmail.com>
2021-02-10 13:39:08 +0100

    Refactor code

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -29,30 +29,40 @@ export type APICharacters = {
   results: APICharacter[];
 };
 
+export type Character = {
+  imgSrc: string;
+  name: string;
+  status: "Alive" | "Dead" | "unknown";
+  species: string;
+  origin: {
+    name: string;
+  };
+};
+
+function convertToCharacter(apiCharacter: APICharacter): Character {
+  return {
+    imgSrc: apiCharacter.image,
+    name: apiCharacter.name,
+    status: apiCharacter.status,
+    species: apiCharacter.species,
+    origin: { name: apiCharacter.origin.name },
+  };
+}
+
 export async function getCharacter(id: number) {
   const response = await fetch(
     `https://rickandmortyapi.com/api/character/${id}`
   );
   const result = (await response.json()) as APICharacter;
-  const character = {
-    imgSrc: result.image,
-    name: result.name,
-    status: result.status,
-    species: result.species,
-    origin: { name: result.origin.name },
-  };
+  const character = convertToCharacter(result);
   return character;
 }
 
 export async function getCharacters() {
   const response = await fetch(`https://rickandmortyapi.com/api/character/`);
   const result = (await response.json()) as APICharacters;
-  const characters = result.results.map((apiCharacter) => ({
-    imgSrc: apiCharacter.image,
-    name: apiCharacter.name,
-    status: apiCharacter.status,
-    species: apiCharacter.species,
-    origin: { name: apiCharacter.origin.name },
-  }));
+  const characters = result.results.map((apiCharacter) =>
+    convertToCharacter(apiCharacter)
+  );
   return characters;
 }
